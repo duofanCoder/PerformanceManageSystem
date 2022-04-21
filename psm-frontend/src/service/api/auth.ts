f6877ba7164ff5ffee2810296bd9c193578773a2
duofan <2441051071@qq.com>
2022-04-21 19:37:15 +0800

    project move

--- psm-frontend/src/service/api/auth.ts
+++ psm-frontend/src/service/api/auth.ts
@@ -1,13 +1,4 @@
-import { mockRequest, request } from '../request';
-
-/**
- * 获取验证码
- * @param phone - 手机号
- * @returns - 返回boolean值表示是否发送成功
- */
-export function fetchSmsCode(phone: string) {
-  return mockRequest.post<boolean>('/getSmsCode', { phone });
-}
+import {  request } from '../request';
 
 /**
  * 登录
@@ -23,12 +14,4 @@ export function fetchLogin(username: string, password: string, type: 'pwd' | 'sm
 /** 获取用户信息 */
 export function fetchUserInfo() {
   return request.get<ApiAuth.UserInfo>('/user/profile');
-}
-
-/**
- * 刷新token
- * @param refreshToken
- */
-export function fetchUpdateToken(refreshToken: string) {
-  return mockRequest.post<ApiAuth.Token>('/updateToken', { refreshToken });
 }
